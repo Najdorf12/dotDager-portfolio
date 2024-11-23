e5abd543af7e2cab8c4db0b513ad30db371f9828
Najdorf12 <agustin.morro@gmail.com>
2024-11-23 14:36:42 -0300

    add styles

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -131,10 +131,10 @@ const Contact = () => {
           <h5 className="text-5xl text-black font-title2 font-medium text-start xl:text-7xl 2xl:text-8xl  ">
             CONTACTO
           </h5>
-          <p className="text-start text-balance text-stone-400 font-title text-sm lg:text-lg 2xl:text-xl  mt-6 xl:w-[70%] xl:mt-9">
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. <span className="text-cyan-600">Quia consequatur quibusdam </span> totam corrupti deserunt eius harum officia voluptates vitae doloremque.
+          <p className="text-start text-balance text-stone-500 font-title text-sm lg:text-lg 2xl:text-xl  mt-6 xl:w-[70%] xl:mt-9">
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. <span className="text-cyan-500 font-medium">Quia consequatur quibusdam </span> totam corrupti deserunt eius harum officia voluptates vitae doloremque.
           </p>
-          <ul className="flex flex-col gap-2 text-sm text-stone-400 xl:text-base font-title2 mt-6 xl:gap-3 xl:mt-7 2xl:mt-9 2xl:text-xl ">
+          <ul className="flex flex-col gap-2 text-sm text-stone-600 xl:text-base font-title2 mt-6 xl:gap-3 xl:mt-7 2xl:mt-9 2xl:text-xl ">
             <a target="_blank" href="https://www.instagram.com/dager.32/">
               <li className="flex justify-start items-center gap-2 lg:gap-3">
                 <i className="bx bxl-instagram text-2xl text-cyan-500 xl:text-3xl 2xl:text-4xl"></i>
